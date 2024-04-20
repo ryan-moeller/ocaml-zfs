4541390ca70f40325c39a97bfb05b28dcf36b4a3
Ryan Moeller <ryan-moeller@att.net>
2024-04-20 10:46:15 -0400

    Add snapshot test

--- test/test_ioctls.ml
+++ test/test_ioctls.ml
@@ -1137,3 +1137,10 @@ let () =
       Printf.eprintf "rename failed\n";
       failwith @@ Unix.error_message e);
   common_cleanup vdevs
+
+(* snapshot *)
+let () =
+  let vdevs = common_setup () in
+  common_dataset_create test_dataset_name;
+  common_snapshot_create test_snapshot_name;
+  common_cleanup vdevs
