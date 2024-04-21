a7731a3b739fdd9d77e667f6bdcabdc1a0972864
Ryan Moeller <ryan-moeller@att.net>
2024-04-20 20:47:43 -0400

    Add a few enum to int conversions

--- lib/util.c
+++ lib/util.c
@@ -7,3 +7,17 @@ caml_zfs_util_int_of_descr(value descr)
 	CAMLparam1 (descr);
 	CAMLreturn (descr);
 }
+
+CAMLprim value
+caml_zfs_util_int_of_pool_scan_func(value func)
+{
+	CAMLparam1 (func);
+	CAMLreturn (func);
+}
+
+CAMLprim value
+caml_zfs_util_int_of_pool_scrub_cmd(value cmd)
+{
+	CAMLparam1 (cmd);
+	CAMLreturn (cmd);
+}
