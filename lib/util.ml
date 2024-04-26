9bd3fb56a8ac529211bfc8fe1bf3480a9630c0f4
Ryan Moeller <ryan-moeller@att.net>
2024-04-26 15:10:08 -0400

    Move Util module into its own file
    This module is expected to grow as additional functionality is
    implemented.

---
+++ lib/util.ml
@@ -0,0 +1,9 @@
+open Types
+
+external int_of_descr : Unix.file_descr -> int = "caml_zfs_util_int_of_descr"
+
+external int_of_pool_scan_func : pool_scan_func -> int
+  = "caml_zfs_util_int_of_pool_scan_func"
+
+external int_of_pool_scrub_cmd : pool_scrub_cmd -> int
+  = "caml_zfs_util_int_of_pool_scrub_cmd"
--- lib/zfs.ml
+++ lib/zfs.ml
@@ -517,12 +517,3 @@ module Zfs_ioctls = struct
     = "caml_zfs_ioc_get_bootenv"
 end
 
-module Util = struct
-  external int_of_descr : Unix.file_descr -> int = "caml_zfs_util_int_of_descr"
-
-  external int_of_pool_scan_func : pool_scan_func -> int
-    = "caml_zfs_util_int_of_pool_scan_func"
-
-  external int_of_pool_scrub_cmd : pool_scrub_cmd -> int
-    = "caml_zfs_util_int_of_pool_scrub_cmd"
-end
