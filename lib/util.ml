a1c260148655fc2661932bc1c8a2f2a831680cff
Ryan Moeller <ryan-moeller@att.net>
2024-04-26 19:05:07 -0400

    Add Util.nicestrtonum
    This function operates in similar principle to zfs_nicestrtonum(),
    though rather than handling up to UINT64_MAX it only accepts up to
    Int64.max_int for floating point input values.

--- lib/util.ml
+++ lib/util.ml
@@ -7,3 +7,41 @@ external int_of_pool_scan_func : pool_scan_func -> int
 
 external int_of_pool_scrub_cmd : pool_scrub_cmd -> int
   = "caml_zfs_util_int_of_pool_scrub_cmd"
+
+let nicestrtonum s =
+  let shiftamt suffix =
+    match String.uppercase_ascii suffix with
+    | "" | "B" -> Some 0
+    | "K" | "KB" | "KIB" -> Some 10
+    | "M" | "MB" | "MIB" -> Some 20
+    | "G" | "GB" | "GIB" -> Some 30
+    | "T" | "TB" | "TIB" -> Some 40
+    | "P" | "PB" | "PIB" -> Some 50
+    | "E" | "EB" | "EIB" -> Some 60
+    | "Z" | "ZB" | "ZIB" -> Some 70
+    | _ -> None
+  in
+  match
+    if String.contains s '.' then
+      Scanf.sscanf_opt s "%f%s" (fun floatval suffix ->
+          match shiftamt suffix with
+          | Some amt ->
+              let finalfloat = floatval *. Float.pow 2.0 (Float.of_int amt) in
+              let maxfloat = Int64.to_float Int64.max_int in
+              if finalfloat > maxfloat then Error "numeric value is too large"
+              else Ok (Int64.of_float finalfloat)
+          | None -> Error (Printf.sprintf "invalid numeric suffix '%s'" suffix))
+    else
+      Scanf.sscanf_opt s "%Lu%s" (fun intval suffix ->
+          match shiftamt suffix with
+          | Some amt ->
+              if amt >= 64 then Error "numeric value is too large"
+              else
+                let finalint = Int64.shift_left intval amt in
+                let unshifted = Int64.shift_right finalint amt in
+                if unshifted != intval then Error "numeric value is too large"
+                else Ok finalint
+          | None -> Error (Printf.sprintf "invalid numeric suffix '%s'" suffix))
+  with
+  | Some result -> result
+  | None -> Error (Printf.sprintf "bad numeric value '%s'" s)
