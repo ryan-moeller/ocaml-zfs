29e3aa8a1c36c0f3d8a7cb9ffc3dbf2109f66546
Ryan Moeller <ryan-moeller@att.net>
2024-05-14 20:11:30 -0400

    Fix style

--- lib/util.ml
+++ lib/util.ml
@@ -77,18 +77,16 @@ let nicestrtonum s =
   | Some result -> result
   | None -> Error (Printf.sprintf "bad numeric value '%s'" s)
 
-let isprint c = c >= Char.chr 0x20 && c <= Char.chr 0x7e
 let max_prop_len = 1024 (* 1024 on FreeBSD, 4096 on Linux *)
 let max_name_len = 256
 let max_comment_len = 32
 let origin_dir_name = "$ORIGIN"
-
-let version_is_supported v = (v >= 1L && v <= 28L) || v = 5000L
-
 let spa_minblockshift = 9
 let spa_minblocksize = Int64.shift_left 1L spa_minblockshift
 let spa_old_maxblockshift = 17
 let spa_old_maxblocksize = Int64.shift_left 1L spa_old_maxblockshift
 let spa_maxblockshift = 24
 let spa_maxblocksize = Int64.shift_left 1L spa_maxblockshift
+let version_is_supported v = (v >= 1L && v <= 28L) || v = 5000L
+let isprint c = c >= Char.chr 0x20 && c <= Char.chr 0x7e
 let ispower2 x = Int64.logand x (Int64.sub x 1L) = 0L
